bfd7b2e7526ae52ae95f675c229eb0c63733ce00
Jeremy Yandell <jeremyyandell@Jeremys-MacBook-Pro.local>
2019-07-02 14:04:53 -0700

    add buttons to html, built game object structure

--- js/app.js
+++ js/app.js
@@ -9,6 +9,33 @@ class Tamagotchi {
         this.boredom = 1;
     }
 }
+
+const game = {
+    timer: 0,
+    startGame()  {
+        //stuff to do when game starts
+    },
+    startTimer()  {
+        //begin the timer
+        //age tamagotchi and starts incrementing the other stuff
+    },
+    killTama()  {
+        //check for conditions that kill tamagotchi
+    },
+    feedTama()  {
+        //things to do when you feed Tama
+    },
+    playWithTama()  {
+        //things to do when playing with Tama
+    },
+    putTamaToBed()  {
+
+    }
+
+}
+
+
+
 const tama = new Tamagotchi('buddyboy');
 
 console.log(tama);

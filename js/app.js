6f3b62d1c2d28e6ab597c4f6cf7d521be08cc5d8
Jeremy Yandell <jeremyyandell@Jeremys-MacBook-Pro.local>
2019-07-03 12:29:53 -0700

    css flexbox styling

--- js/app.js
+++ js/app.js
@@ -10,25 +10,28 @@ class Tamagotchi {
     }
 }
 //variables to work with
-const buttonConsole = document.querySelector('#btnContainer');
+const buttonConsole = document.querySelector('#btn-container');
 const startButton = document.querySelector('#start');
 const feedButton = document.querySelector('#feed');
 const playButton = document.querySelector('#play');
 const sleepButton = document.querySelector('#sleep');
 const pauseButton = document.querySelector('#pause');
 const stats = document.querySelector('#stats');
+const lair = document.querySelector('#tama-lair');
 
 //game object
 const game = {
     paused: false,
     timer: 0,
     tamaArray: [],
+    currentImg: "https://i.imgur.com/5gPZZ2z.png?1",
     startGame()  {
         console.log('start game');
         const tama = new Tamagotchi('buddyboy');
         game.tamaArray.push(tama);
         this.startTimer();
         this.updateStats();
+        this.updateImg();
     },
     startTimer()  {
         this.paused = false;
@@ -36,6 +39,15 @@ const game = {
         this.timer ++;
         if (this.timer % 10 == 0)  {
             this.tamaArray[0].age++;
+        }
+        if (this.timer === 30) {
+            this.currentImg = "https://i.imgur.com/CNZisda.png?1";
+            this.updateImg();
+        }
+        if (this.timer % 15 == 0)  {
+            this.tamaArray[0].hunger ++;
+            this.tamaArray[0].sleepy ++;
+            this.tamaArray[0].boredom ++;
         }
         if (this.paused == true)  {
             clearInterval(timer);
@@ -52,17 +64,28 @@ const game = {
     updateStats()  {
         stats.innerText = `Timer: ${game.timer} Age: ${this.tamaArray[0].age} Hunger: ${this.tamaArray[0].hunger} Sleepy: ${this.tamaArray[0].sleepy} Boredom: ${this.tamaArray[0].boredom}`;
     },
+    updateImg()  {
+        const img = document.createElement('img');
+        img.setAttribute('src', this.currentImg);
+        while (lair.firstChild) {
+            lair.removeChild(lair.firstChild);
+        }
+        lair.appendChild(img);
+    },
     killTama()  {
         //check for conditions that kill tamagotchi
     },
     feedTama()  {
         console.log('Give Tama some food');
+        this.tamaArray[0].hunger --;
     },
     playWithTama()  {
         console.log('Play with Tama');
+        this.tamaArray[0].boredom --;
     },
     putTamaToBed()  {
         console.log('Time for bed');
+        this.tamaArray[0].sleepy --;
     }
 
 }
